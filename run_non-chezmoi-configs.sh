54f35afab5b748263e9aecac9332e1fa95201bbd
1inguini <9647142@gmail.com>
2021-10-11 09:01:29 +0900

    correct management of chezmoi config & script to install non-chezmoi configs

---
+++ run_non-chezmoi-configs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+function GitUpdate () {
+  git clone "$1" "$2" \
+    || { cd "$2"; git pull; }
+}
+
+# nvim
+GitUpdate https://github.com/1inguini/init.vim.git "$XDG_CONFIG_HOME"/nvim
+
+# xmonad
+GitUpdate https://github.com/1inguini/.xmonad.git ~/.xmonad
+xmonad --recompile && xmonad --restart
